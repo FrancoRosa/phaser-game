398f2dae0cc3ba3dfc5529395b35ca185354eb71
FrancoRosa <km115.franco@gmail.com>
2020-09-07 10:43:26 -0500

    Add update function and define keys

--- js/SceneMain.js
+++ js/SceneMain.js
@@ -31,6 +31,12 @@ class SceneMain extends Phaser.Scene {
   }
 
   create() {
+    this.keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+    this.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+    this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+    this.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+    this.keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+
     this.anims.create({
       key: "sprEnemy0",
       frames: this.anims.generateFrameNumbers("sprEnemy0"),
@@ -66,5 +72,32 @@ class SceneMain extends Phaser.Scene {
       ],
       laser: this.sound.add("sndLaser")
     };
+
+    this.player = new Player(
+      this,
+      this.game.config.width * 0.5,
+      this.game.config.height * 0.5,
+      "sprPlayer"
+    ); 
+    
+    
+  }
+
+  update() {
+    this.player.update();
+
+    if (this.keyW.isDown) {
+      this.player.moveUp();
+    }
+    else if (this.keyS.isDown) {
+      this.player.moveDown();
+    }
+
+    if (this.keyA.isDown) {
+      this.player.moveLeft();
+    }
+    else if (this.keyD.isDown) {
+      this.player.moveRight();
+    }
   }
 }
